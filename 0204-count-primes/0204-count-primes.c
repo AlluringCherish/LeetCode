da7e1fa8f2e3b431c887d180c342bb8374151f39
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-09-20 15:34:37 +0900

    Time: 400 ms (5.19%), Space: 27.1 MB (62.28%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.c
@@ -0,0 +1,17 @@
+int IsPrime[5000001];
+
+int countPrimes(int n) {
+    int count=0;
+    for (int i=2; i*i<=n ; i++){
+        for (int j=i ; i*j<=n; j++){
+            IsPrime[i*j]=1; // Number i*j is Not Prime
+        }
+    }
+    
+    for (int z=2; z<n ; z++){
+        if (!IsPrime[z]){
+            count++;
+        }
+    }
+    return count;
+}

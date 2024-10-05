7c45b967b28c8ea85492cda21db846694c6a1703
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-10-05 20:33:07 +0900

    Time: 23 ms (82.67%), Space: 12.6 MB (92.69%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.c
@@ -0,0 +1,28 @@
+#include <limits.h>
+
+int minSubArrayLen(int target, int* nums, int numsSize) {
+    int min = INT_MAX, sum = 0, i=0, j = 0;
+    
+    for (;j<numsSize;j++){
+        sum+=nums[j];
+        while (sum >= target){
+            if (sum >= target){
+                if (min > j-i+1){
+                    min = j-i+1;
+                }
+            }
+            
+            sum -=nums[i];
+            i++;
+            
+        }
+    }
+        
+    
+    if (min == INT_MAX){
+        return 0;
+    }
+    else{
+        return min;
+    }
+}

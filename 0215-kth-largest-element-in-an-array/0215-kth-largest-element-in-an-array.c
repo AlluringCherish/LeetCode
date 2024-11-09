727e6503fa8007407380628215a2ae3e3b8c6bd1
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-11-09 16:04:53 +0900

    Time: 4 ms (86.83%), Space: 13.9 MB (87.94%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.c
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
-
+//sol1: Qsort
+/*
 int compare(const void *a, const void *b){
     return (*(int*)b-*(int*)a);
 }
@@ -12,4 +13,37 @@ int findKthLargest(int* nums, int numsSize, int k) {
     
     return nums[k-1];
     
+}
+*/
+
+//sol2: counting
+
+int findKthLargest(int* nums, int numsSize, int k) {
+    int arr[20002];
+    for (int i=0; i<20002; i++){
+        arr[i]=0;
+    }
+    
+    for (int i=0; i<numsSize;i++){
+        arr[nums[i]+10001]++;
+    }
+    
+    int i=20001;
+    while(1){
+        while(arr[i]){
+            if (k>1){
+                
+                k--;
+                arr[i]-=1;
+            }
+            else if (k==1){
+                return (i-10001);
+                
+            }
+        }
+        i--;
+    }
+    return 0;    
+
+     
 }

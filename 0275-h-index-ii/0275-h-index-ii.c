a74557763ea0a9831d130e14c150986fd5fab503
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-11-17 19:41:08 +0900

    Time: 0 ms (100.00%), Space: 11.4 MB (80.85%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.c
+++ 0275-h-index-ii/0275-h-index-ii.c
@@ -1,4 +1,6 @@
+/* Sol1: Time Complexity is O(n)
 int hIndex(int* citations, int citationsSize) {
+    // Time Complexity is O(n)
     int i=citationsSize-1;
     int j=1;
     for(;i>=0;i--,j++){
@@ -8,4 +10,25 @@ int hIndex(int* citations, int citationsSize) {
     }
     
     return j-1;
+}
+*/
+
+
+
+
+int hIndex(int* citations, int citationsSize) {
+    int l=0, r=citationsSize-1,ans=0;
+    while(l<=r){
+        int mid = l+(r-l)/2;
+        if ((citationsSize-mid)<=citations[mid]){
+            ans = (citationsSize-mid);
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    
+    
+    return ans;
 }

cd2f6bad11a9d489419ceb1d2a870b6e529a59e8
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-11-27 23:26:39 +0900

    Time: 0 ms (100.00%), Space: 8 MB (18.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -1,9 +1,26 @@
+
+int dp[46];
+
 int climbStairs(int n) {
-    int a=1, b=1, c=1;
-    for (int i=1 ; i<n ; i++){
-        c=a+b;
-        a=b;
-        b=c;
+    for (int i=0; i<46; i++){
+        dp[i]=0;
     }
-    return c;
+    
+    
+    dp[1]=1;
+    dp[2]=2;
+    
+    if (n<=2){
+        return dp[n];
+    }
+    
+    for (int i=3; i<=n;i++){
+        dp[i]=dp[i-2]+dp[i-1];
+    }
+    
+    return dp[n];
+    
+    
+    
+    
 }

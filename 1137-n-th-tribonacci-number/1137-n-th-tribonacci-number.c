2a64270cfe1f48c37c15e3e0a4906d03da79faee
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-11-28 12:22:16 +0900

    Time: 0 ms (100.00%), Space: 7.7 MB (54.27%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.c
@@ -0,0 +1,22 @@
+
+int dp[38];
+
+
+int tribonacci(int n) {
+    for (int i=0; i<38; i++){
+        dp[i]=0;
+    }
+    
+    
+    dp[0]=0;
+    dp[1]=1;
+    dp[2]=1;
+    
+    for (int i=3; i<=n; i++){
+        dp[i]=dp[i-3]+dp[i-2]+dp[i-1];
+    }
+    
+    return dp[n];
+    
+    
+}

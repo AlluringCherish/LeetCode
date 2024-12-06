10533dc8c20b6ded7d9423113f677874ccff0ea1
leejunyong <163401658+AlluringCherish@users.noreply.github.com>
2024-12-06 22:31:04 +0900

    Time: 684 ms (5.42%), Space: 9.5 MB (74.37%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.c
+++ 0072-edit-distance/0072-edit-distance.c
@@ -1,6 +1,8 @@
 #include <string.h>
-#define min(a,b) ((a)<(b)? (a):(b))
-int dp[501][501]; // base 1-index, i번째 글자로 j번째 글자를 만드는 최소 비용
+#define min(a,b) ((a)>(b) ? (b):(a))
+
+
+int dp[501][501]; // dp[i][j].. a의 i번째글자에서 b의 j번째글자까지 최소회수
 
 int minDistance(char* word1, char* word2) {
     for (int i=0; i<501; i++){
@@ -9,28 +11,33 @@ int minDistance(char* word1, char* word2) {
         }
     }
     
-    
+    int a = strlen(word1), b = strlen(word2);
     dp[0][0]=0;
-    for (int j=0; j<=strlen(word2); j++){
-        dp[0][j]=j;
-    }
-    
-    for (int i=0; i<=strlen(word1); i++){
+    for (int i=1; i<=a; i++){
         dp[i][0]=i;
     }
+    for (int i=1; i<=b; i++){
+        dp[0][i]=i;
+    }
     
-    for (int i=1; i<=strlen(word1); i++){
-        for (int j=1 ; j<=strlen(word2); j++){
+    for (int i=1; i<=a; i++){
+        for (int j=1; j<=b; j++){
             if (word1[i-1]==word2[j-1]){
                 dp[i][j]=dp[i-1][j-1];
             }
             else{
-                dp[i][j]=min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1;
+                dp[i][j]=min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
             }
-            
         }
     }
     
-    return dp[strlen(word1)][strlen(word2)];
+    for (int i=0; i<=a; i++){
+        for (int j=0; j<=b; j++){
+            printf("%d ",dp[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return dp[a][b];
     
 }
